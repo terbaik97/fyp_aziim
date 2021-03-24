b29e8c445d8d935bdbdfa9766cedf027357ce9ef
Muhamad Aziim Abdul Halim <aziimmashoor1997@gmail.com>
2021-03-24 23:27:00 +0800

    Add UserAction table with version and relationship

--- app/models/user_action.rb
+++ app/models/user_action.rb
@@ -1,10 +1,11 @@
 class UserAction < ApplicationRecord
-  has_paper_trail
+  has_paper_trail versions: { class_name: "PaperTrail::UserActionVersion" }
   enum status: {
       active: 0,
       disabled: 1
     }
 
-  belongs_to :user, class_name: 'User'
-  has_many :pois , foreign_key: :poi_id, class_name: 'Poi'
+  belongs_to :user
+  belongs_to :poi
+
 end
