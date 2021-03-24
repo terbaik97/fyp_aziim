e42e249fcceff7f0d46df9d0dfd63c2eb7947e45
Muhamad Aziim Abdul Halim <aziimmashoor1997@gmail.com>
2021-03-24 23:26:02 +0800

    Create Poi controller for index , create , update , show and show version

--- app/controllers/api/v1/pois_controller.rb
+++ app/controllers/api/v1/pois_controller.rb
@@ -1,43 +1,76 @@
 module Api
     module V1
         class PoisController < ApplicationController
-            skip_before_action :authorize_request
+            skip_before_action :authorize_request, only: [:show,:show_version]
+            before_action :set_paper_trail_whodunnit
+            def index
+              
+            end
 
             def create
               if request.headers["Authorization"].present?
                 authorize_request
-                user_action = UserAction.new(action_user: "created", user_id: authorize_request.id)
-                create_poi = Poi.new(:user_id => authorize_request.id , :fields => params[:fields] , :coordinate => params[:coordinate ])
                 
-                if !user_action.save
-                  return json_response(message: user_action.errors.full_messages, status: :unprocessable_entity)
-                end
-                if !create_poi.save
-                  return json_response(message: create_poi.errors.full_messages, status: :unprocessable_entity)
-                end
-                return_msg = 'Successfully created poi'
-                json_response(
+                
+                create_poi = Poi.new(:user_id => authorize_request.id , :fields => params[:fields] , :coordinate => params[:coordinate ])
+                # create_poi.user_actions.build(action_user: "create", user_id: authorize_request.id)
+                if create_poi.save
+                  user_action = UserAction.new(action_user: "create", user_id: authorize_request.id , poi_id: create_poi.id)
+                  user_action.save
+                else
+                  return_msg = 'Not successfully create poi'
+                  json_response(
                     message: return_msg
                   )
+                end
+                render json: create_poi
                 
               end
               
             end
 
             def update
-              user_action = UserAction.new(action_user: "update", user_id: authorize_request.id)
-              update_poi = Poi.find(params[:poi_id])
               
+              update_poi = Poi.find(params[:poi_id])
               if update_poi.update(:fields => params[:fields] , :coordinate => params[:coordinate ] )
+                user_action = UserAction.new(action_user: "update", user_id: authorize_request.id , poi_id: update_poi.id)
+                user_action.save
                 render json: update_poi
               else
-                return_msg = 'Successfully created poi'
+                return_msg = 'Not successfully update poi'
                 json_response(
                     message: return_msg
                   )
               end
             end
 
+            def show
+             
+              find_poi = Poi.find(params[:id])
+              if find_poi
+                render json: find_poi 
+              else
+                return_msg = 'Not successfully find poi'
+                json_response(
+                    message: return_msg
+                  )
+              end
+            end
+
+            def show_version
+             
+              find_poi = Poi.find(params[:id])
+              if find_poi
+                render json: find_poi.versions
+              else
+                return_msg = 'Not successfully find poi version'
+                json_response(
+                    message: return_msg
+                  )
+              end
+              
+             
+            end
             private
         
             def poi_params 
