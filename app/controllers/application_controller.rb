9038f6098e4e0c833502d26ef753fbc56a19c0e0
Muhamad Aziim Abdul Halim <aziimmashoor1997@gmail.com>
2021-04-08 21:59:44 +0800

    Adding error handler

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,14 @@
 class ApplicationController < ActionController::API
     include Response
     include ExceptionHandler
-    
+    include ErrorHandler
     before_action :authorize_request
-  
+    
     attr_reader :current_user
   
     def authorize_request
       @current_user = Services::Auth::AuthorizeReq.call!(request.headers).data
     end
+
+    # include Error::ErrorHandler
 end
