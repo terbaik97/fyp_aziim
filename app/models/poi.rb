e395f2ac3dd874a82d3151c698d09da14ff1d017
Muhamad Aziim Abdul Halim <aziimmashoor1997@gmail.com>
2021-04-19 14:50:03 +0800

    Fix geolocation and adding search poi_version

--- app/models/poi.rb
+++ app/models/poi.rb
@@ -1,6 +1,7 @@
 class Poi < ApplicationRecord
   has_paper_trail versions: { class_name: "PaperTrail::PoiVersion" }
   has_many :user_actions
+  has_many :image_pois
   validates_presence_of  :name
   acts_as_mappable  :default_units => :miles,
                     :default_formula => :sphere,
