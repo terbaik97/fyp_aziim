54cbfc4f2e96a40352d2816eb39ad8b7e51e34e3
terbaik97 <aziimmashoor1997@gmail.com>
2021-05-25 15:15:26 +0800

    Adding images upload by the application

---
+++ app/controllers/api/v1/user_actions_controller.rb
@@ -0,0 +1,71 @@
+module Api
+    module V1
+        class UserActionsController < ApplicationController
+
+          def user_action
+            user =  UserAction.where(user_id: params[:id])
+            json_response(
+              data: user
+            )
+          end
+
+          def user_point
+            user = User.find_by(id: params[:id])
+            
+            if user
+              if params[:user_point].present?
+                user.update(:user_point => params[:user_point].to_i)
+                end
+                json_response(
+                  message: "Successfully update point",
+                  data: user
+                )
+            else
+              json_response(
+                message: "Not successfully update point"
+              )
+            end
+
+          end
+
+          def add_user_point
+            user = User.find_by(id: params[:id])
+
+            if user
+              if params[:user_point].present?
+                user.update(:user_point => user.user_point + params[:user_point].to_i)
+                end
+                json_response(
+                  message: "Successfully update point",
+                  data: user
+                )
+            else
+              json_response(
+                message: "Not successfully update point"
+              )
+            end
+
+          end
+
+          def subtract_user_point
+            user = User.find_by(id: params[:id])
+
+            if user
+              if params[:user_point].present?
+                user.update(:user_point => user.user_point - params[:user_point].to_i)
+                end
+                json_response(
+                  message: "Successfully update point",
+                  data: user
+                )
+            else
+              json_response(
+                message: "Not successfully update point"
+              )
+            end
+
+          end
+
+        end
+    end
+end
